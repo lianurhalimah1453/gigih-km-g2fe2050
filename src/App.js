3865c28101c182e42f32a776d9185e686b447975
lianurhalimah1453 <halimahlianur@gmail.com>
2022-03-23 13:14:34 +0700

    Ngepush Tugas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import data from './data/single-sample';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<section>
+      <div className="container">
+        <div className="song">
+          <div className="song-img">
+            <img src={data.album.images[0].url} className="App-logo" alt="logo" />
+          </div>
+          <div className="song-desc">
+            <p>{data.album.name}</p>
+            <p>{data.artists[0].name}</p>
+            <button>Select</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
